9178b73d2459335f31f8c97ecb33365b9d0cbb9e
janjanurjaman <85498714+janjanurjaman@users.noreply.github.com>
2022-05-28 21:53:11 +0700

    Add files via upload

---
+++ kecilKbesar.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.janjan.kawahedukasi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author janjan
+ */
+public class kecilKbesar {
+    public static void main(String[] args) {
+        int[] deret = new int[7];
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Input = ");
+        for (int i = 0; i < deret.length; i++) {
+            deret[i] = input.nextInt();           
+        }
+        
+        Arrays.sort(deret);
+        for (int index:deret){
+            System.out.print(" "+index);
+        }
+    }   
+}
